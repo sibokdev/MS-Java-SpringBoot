c7ab1267f65381300ea3a96aa10b7354ab79c7e6
Alfredo Suárez Romero <aromero@freewiretech.com>
2023-08-04 20:31:52 -0600

    Adding docker internal host

--- users-ms/src/main/java/org/sibokdev/springcloud/ms/users/integration/clients/CourseClientRest.java
+++ users-ms/src/main/java/org/sibokdev/springcloud/ms/users/integration/clients/CourseClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="courses-ms", url="localhost:8002")
+@FeignClient(name="courses-ms", url="host.docker.internal:8002")
 public interface CourseClientRest {
     @DeleteMapping("/delete-course-user/{id}")
     void deleteCourseUser(@PathVariable Long id);
